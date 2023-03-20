72f726cbceba2835f345fe74a9096ad5e9a46a7d
Rayen Michael <rayenmike2010@gmail.com>
2023-03-20 20:27:50 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   images: {
-    domains: ['res.cloudinary.com', 'api.nixlab.co.in', 'www.nixlab.co.in'],
+    domains: [
+      "res.cloudinary.com",
+      "https://social-media-api-nodejs.ryanmichaelll.repl.co/",
+      "www.nixlab.co.in",
+    ],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512, 768, 1024, 1536, 2048],
   },
 };
